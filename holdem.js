7bb821a64e6809f2c5475d7d858da15c4324d56c
Bindi <bindi.zh@gmail.com>
2014-05-13 16:42:53 -0700

    setup message listener

--- holdem.js
+++ holdem.js
@@ -1,2 +1,23 @@
 var deck = require('./deck.js');
 
+var io;
+var gameSocket;
+
+/**
+ * init the holdem game
+ * set up message listener
+ *
+ */
+function initGame(sio, socket) {
+    io = sio;
+    gameSocket = socket;
+    gameSocket.emit('connected', {message: "You are connected!"});
+
+    gameSocket.on('hostCreateNewGame', hostCreateNewGame);
+    gameSocket.on('hostNextRound', hostNextRound);
+
+    gameSocket.on('playerJoinGame', playerJoinGame);
+};
+
+
+exports.initGame = initGame;
